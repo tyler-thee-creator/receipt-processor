82b3deee541ce9b6ede4f1993581ce36a5d7cb1d
Tyler Brown <tyler.s.brown.93@gmail.com>
2024-12-12 15:49:32 -0800

    implement receipt processor class

--- src/receipt-processor.test.ts
+++ src/receipt-processor.test.ts
@@ -1,7 +1,51 @@
 import { describe, it, expect } from 'vitest';
+import { validate } from 'uuid';
 
-describe('Math Tests', () => {
-  it('adds two numbers', () => {
-    expect(1 + 1).toBe(2);
+import ReceiptProcessor, { Receipt } from './receipt-processor';
+import calculateReceiptPoints from './points-calculator';
+
+const receipt: Receipt = {
+  retailer: 'Target',
+  purchaseDate: '2022-01-01',
+  purchaseTime: '13:01',
+  items: [
+    {
+      shortDescription: 'Mountain Dew 12PK',
+      price: '6.49'
+    },{
+      shortDescription: 'Emils Cheese Pizza',
+      price: '12.25'
+    },{
+      shortDescription: 'Knorr Creamy Chicken',
+      price: '1.26'
+    },{
+      shortDescription: 'Doritos Nacho Cheese',
+      price: '3.35'
+    },{
+      shortDescription: '   Klarbrunn 12-PK 12 FL OZ  ',
+      price: '12.00'
+    }
+  ],
+  total: '35.35'
+};
+
+describe('ReceiptProcessor', () => {
+  it('Should return a valid uuid after processing a receipt', () => {
+    const receiptProcessor = new ReceiptProcessor();
+    const id = receiptProcessor.processReceipt(receipt);
+    expect(validate(id)).toBe(true);
+  });
+  it('Should correctly return the points for a processed receipt based on the id provided upon receipt processing', () => {
+    const receiptProcessor = new ReceiptProcessor();
+    const id = receiptProcessor.processReceipt(receipt);
+    const points = receiptProcessor.getReceiptPoints(id);
+    expect(points).toBeTypeOf('number');
+  });
+  it('Should correctly calculate receipt points and store the result in the receipt log', () => {
+    const receiptProcessor = new ReceiptProcessor();
+    const expectedPoints = calculateReceiptPoints(receipt);
+    const id = receiptProcessor.processReceipt(receipt);
+    const points = receiptProcessor.getReceiptPoints(id);
+    expect(points).toBe(expectedPoints);
   });
 });
--- src/receipt-processor.ts
+++ src/receipt-processor.ts
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
+import calculateReceiptPoints from './points-calculator';
+
 export interface ReceiptItem {
   shortDescription: string;
   price: string;
@@ -12,3 +14,30 @@ export interface Receipt {
   items: ReceiptItem[];
   total: string;
 }
+
+interface ReceiptPointsLogItem {
+  id: string;
+  points: number;
+}
+
+class ReceiptProcessor {
+  private receiptPointsLog: ReceiptPointsLogItem[];
+
+  constructor() {
+    this.receiptPointsLog = [];
+  }
+
+  processReceipt(receipt: Receipt): string {
+    const id = uuidv4();
+    const points = calculateReceiptPoints(receipt);
+    this.receiptPointsLog.push({ id, points });
+    return id;
+  }
+
+  getReceiptPoints(id: string): number | undefined {
+    const logItem = this.receiptPointsLog.find(x => x.id === id);
+    return logItem?.points;
+  }
+}
+
+export default ReceiptProcessor;
